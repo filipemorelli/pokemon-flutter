b68712751639273dac52cbda85b8836ee5df754c
Filipe Morelli <morellitecinfo@gmail.com>
2020-02-27 22:03:45 -0300

    Retirando PopupMenu

--- lib/globals/routes.dart
+++ lib/globals/routes.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:pokemon_flutter/pages/404/not_found_screen.dart';
 import 'package:pokemon_flutter/pages/pokemon_details/pokemon_details_screen.dart';
 import 'package:pokemon_flutter/pages/pokemons/pokemons_screen.dart';
-import 'package:pokemon_flutter/pages/settings/settings_sceen.dart';
 
 Route<dynamic> onGeneratedRoutes(RouteSettings settings) {
   switch (settings.name) {
@@ -14,10 +13,6 @@ Route<dynamic> onGeneratedRoutes(RouteSettings settings) {
       return MaterialPageRoute(
           builder: (_) => PokemonDetailScreen(pokemon: settings.arguments),
           settings: settings);
-      break;
-    case "settings":
-      return MaterialPageRoute(
-          builder: (_) => SettingsSceen(), settings: settings);
       break;
     default:
       return MaterialPageRoute(
--- lib/pages/pokemons/pokemons_screen.dart
+++ lib/pages/pokemons/pokemons_screen.dart
@@ -37,7 +37,6 @@ class _PokemonsScreenState extends State<PokemonsScreen> {
                 icon: Icon(Icons.search),
                 onPressed: () {},
               ),
-              PopupMenuButtonPokemons()
             ],
           ),
           body: SafeArea(
